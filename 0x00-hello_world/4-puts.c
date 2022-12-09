39b9e546b29860eddeb286443a461eeef1b46e69
d-brightmind <robiuridwanullah123@gmail.com>
2022-12-09 05:12:02 +0100

    4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /*                                                                     
  * main - Entry                                                                                                                         
  * Return: Always 0                                                                                                                     
- */                                                                                                                                     
+ */                                                                
 int main(void)                                                                                                                          
 {                                                                                                                                       
-        puts("\"Programming is like building a multi lingual puzzle);                                                            
+        puts("\"Programming is like building a multi lingual puzzle");                                                            
         return (0);
 }
