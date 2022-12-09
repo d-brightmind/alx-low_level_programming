6c7d56d2f824ccc71a717d4e23acb7a53006cdb6
d-brightmind <robiuridwanullah123@gmail.com>
2022-12-09 05:35:25 +0100

    5-puts.c file

--- 0x00-hello_world/printf.c
+++ 0x00-hello_world/printf.c
@@ -1,5 +1,5 @@
 #include <stdio.h>                                                                                                                      
-/**                                                                                                                                     
+/*                                                                                                                                    
  * main - Entry                                                                                                                         
  * Return: Always 0                                                                                                                     
  */ 
