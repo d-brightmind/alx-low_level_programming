a9e1f79d5ab5b2bd04b7ac9e95cc9a6da75a9978
d-brightmind <robiuridwanullah123@gmail.com>
2022-12-11 00:51:59 -0800

    3-print_alphabets.c file

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,42 +2,34 @@
 
 /**
  *
- *  * main - print if the number is postive, zero, or negative
+ *	* main - print if the number is postive, zero, or negative
  *
- *   *
+ *		*
  *
- *    * Description: using the main function
+ *			* using the main function
  *
- *     * this program prints "Programming is positive, zero, or negative
+ *				* this program prints "Programming is positive, zero, or negative
  *
- *      * Return: 0
- *
- *       */
+ *					* Return: 0
+ */
 
 int main(void)
 
-{
-
-	char ch;
-
-	for (ch = 'a' ; ch <= 'z' ; ch++)
-
 	{
 
+		char ch;
+
+		for (ch = 'a' ; ch <= 'z' ; ch++)
+
+		{
 			putchar(ch);
+		}
+		for (ch = 'A' ; ch <= 'Z' ; ch++)
 
-	}
-
-	for (ch = 'A' ; ch <= 'Z' ; ch++)
-
-	{
-
+		{
 			putchar(ch);
+		}
 
+			putchar('\n');
+			return (0);
 	}
-
-	putchar('\n');
-
-	return (0);
-
-}
