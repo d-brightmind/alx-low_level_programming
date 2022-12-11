788d4c940d744015a5321347694842c046ff09db
d-brightmind <robiuridwanullah123@gmail.com>
2022-12-11 03:11:05 -0800

    101-quote.c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
-    #include <unistd.h>                                                                                                                      
-/** 
- * main - Entry
- * Return: Always 1
-*/
+#include <unistd.h>
+
+/**
+ *	* main - Entry
+ *
+ *	* Return: Always 1
+ */
+
 int main(void)
-{
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
-}
+	{
+		write(1,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			59);
+		return (1);
+	}
