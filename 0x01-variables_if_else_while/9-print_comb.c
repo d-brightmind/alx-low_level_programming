6696099ab2a59a8b45255296a9a5eae8285a4e06
d-brightmind <robiuridwanullah123@gmail.com>
2022-12-11 01:47:43 -0800

    9-print_comb.c file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -18,14 +18,13 @@ int main(void)
 
 		for (c = 0; c <= 9; c++)
 		{
-			putchar(c);
-		if (c != 9)
-		{
+			putchar((c % 10) + '0');
+			if (c == 9)
+				continue;
 			putchar(',');
 			putchar(' ');
-		}
 
 		}
 			putchar('\n');
-		return 0;
+		return (0);
 	}
