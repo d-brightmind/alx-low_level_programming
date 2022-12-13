2a2df0194a24aac3110f34686da6d219184fe5df
d-brightmind <robiuridwanullah123@gmail.com>
2022-12-13 21:43:59 +0100

    4-isalpha.c file

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
  *	* _isalpha - Checks if a character is alphabetic.
- * 	* @c: The character to be checked.
+ *	* @c: The character to be checked.
  *
  *	* Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
  */
