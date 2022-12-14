2d171f7f74f2554a32fa27bca740b2670554991e
d-brightmind <robiuridwanullah123@gmail.com>
2022-12-14 05:24:19 +0100

    104-fibonacci.c main file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,18 +13,18 @@ int main(void)
 		int i;
 
 		for (i = 1; i<= 98; i++)
-		{
-			s = t0 + t1;
+			{
+				s = t0 + t1;
 			if (i != 98)
-                	{
-				printf("%lu, ", s);
-                	}
+                		{
+					printf("%lu, ", s);
+                		}
                 	else
-                	{
-				printf("%lu\n", s);
-                	}
-                		t0 = t1;
-                		t1 = s;
-        		}
+                		{
+					printf("%lu\n", s);
+                		}
+                			t0 = t1;
+                			t1 = s;
+        			}
 			return (0);
 	}
