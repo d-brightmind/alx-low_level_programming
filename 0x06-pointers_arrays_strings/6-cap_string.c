59898cb16b797b0e7d8c23811494a58b7fa0e866
d-brightmind <robiuridwanullah123@gmail.com>
2022-12-21 19:55:46 +0100

    6-cap_string.c file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,8 +20,8 @@ char *cap_string(char *s)
 				{
 					for (j = 0; j <= 12; j++)
 					{
-					if (a[j] == *(s + i - 1))
-							*(s + i) -= 'a' - 'A';
+						if (a[j] == *(s + i - 1))
+	*(s + i) -= 'a' - 'A';
 					}
 				}
 			}
