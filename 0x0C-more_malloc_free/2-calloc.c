538f253cc3cb81a41de034ac88ad030695ce2df7
d-brightmind <robiuridwanullah123@gmail.com>
2023-01-12 15:50:52 +0100

    update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 		if (nmemb == 0 || size == 0)
 			return (NULL);
-			a = malloc(nmemb * size);
+		a = malloc(nmemb * size);
 		if (a == NULL)
 			return (NULL);
 		for (b = 0; b < (nmemb * size); b++)
