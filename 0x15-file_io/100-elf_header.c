8d08a020ad03f4eb1ed4928d3aad57a0b814c5af
d-brightmind <robiuridwanullah123@gmail.com>
2023-02-07 05:30:27 +0100

    update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -254,7 +254,7 @@ void close_elf(int elf)
 		if (close(elf) == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't close fd %d\n", elf);
+			"Error: Can't close fd %d\n", elf);
 			exit(98);
 		}
 	}
