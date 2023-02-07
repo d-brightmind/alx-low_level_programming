3a5b1f3a95056b709ea8503dd9dc279476810227
d-brightmind <robiuridwanullah123@gmail.com>
2023-02-07 05:55:19 +0100

    update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -31,8 +31,8 @@ void check_elf(unsigned char *e_ident)
 		{
 			if (e_ident[index] != 127 &&
 			e_ident[index] != 'E' &&
-		    	e_ident[index] != 'L' &&
-		    	e_ident[index] != 'F')
+			e_ident[index] != 'L' &&
+			e_ident[index] != 'F')
 			{
 				dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
 				exit(98);
@@ -183,7 +183,7 @@ void print_osabi(unsigned char *e_ident)
 void print_abi(unsigned char *e_ident)
 	{
 		printf("  ABI Version:                       %d\n",
-		e_ident[EI_ABIVERSION]);
+			e_ident[EI_ABIVERSION]);
 	}
 
 /**
@@ -217,8 +217,8 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 				break;
 			default:
 				printf("<unknown: %x>\n", e_type);
+			}
 		}
-	}
 
 /**
  *	print_entry - Prints the entry point of an ELF header.
@@ -227,12 +227,13 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
  */
 void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 	{
-		printf("  Entry point address:               ");
+		printf("  Entry point address:   
+				");
 
-		if (e_ident[EI_DATA] == ELFDATA2MSB)
+			if (e_ident[EI_DATA] == ELFDATA2MSB)
 		{
 			e_entry = ((e_entry << 8) & 0xFF00FF00) |
-			((e_entry >> 8) & 0xFF00FF);
+			  	((e_entry >> 8) & 0xFF00FF);
 			e_entry = (e_entry << 16) | (e_entry >> 16);
 		}
 
@@ -254,7 +255,7 @@ void close_elf(int elf)
 		if (close(elf) == -1)
 		{
 			dprintf(STDERR_FILENO,
-			"Error: Can't close fd %d\n", elf);
+				"Error: Can't close fd %d\n", elf);
 			exit(98);
 		}
 	}
@@ -277,6 +278,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		int o, r;
 
 		o = open(argv[1], O_RDONLY);
+
 		if (o == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
@@ -292,10 +294,10 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		r = read(o, header, sizeof(Elf64_Ehdr));
 		if (r == -1)
 		{
-		free(header);
-		close_elf(o);
-		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
-		exit(98);
+			free(header);
+			close_elf(o);
+			dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
+			exit(98);
 		}
 
 		check_elf(header->e_ident);
